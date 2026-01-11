0d84b37820108ecd74a4ddbc585663016a12aa9d
blackanvilsoftworks <blackanvilsoftworks@gmail.com>
2026-01-11 17:38:43 -0300

    Se traen los archivos de imagenes para pruebas en dev

--- src/components/layout/Menu.jsx
+++ src/components/layout/Menu.jsx
@@ -7,7 +7,7 @@ const Menu = () => {
         <nav id="navbar_container" className="navbar navbar-expand-md navbar-light m-0 p-0 fixed-top shadow">
             <div className="container-fluid">
                 <a className="navbar-brand" href="#hero_container">
-                    <img className="d-inline-block align-text-top me-2" src="./assets/navbar-logo.png" alt="calwill &amp; fiorella Logo" height="40" />
+                    <img className="d-inline-block align-text-top me-2" src="./public/navbar-logo.png" alt="calwill &amp; fiorella Logo" height="40" />
                     <span className="ms-2 navbar-title">CALWILL &amp; FIORELLA</span>
                 </a>
                 <button className="navbar-toggler border-0" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" type="button">
