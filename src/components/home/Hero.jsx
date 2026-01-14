b7a00d6edc2a2bea1e6e17c9ba6ec0b69a52036e
blackanvilsoftworks <blackanvilsoftworks@gmail.com>
2026-01-14 14:12:51 -0300

    Se adecúa el componente Hero para el uso de .scss

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -6,7 +6,6 @@ const Hero = () => {
         <div id="hero_container" className="hero mt-5 px-3">
             <h1 className="display-4 fw-bold mb-4">Calzados para Toda la Familia</h1>
             <p className="lead mb-4 fw-semibold">Somos distribuidores de una gran variedad de modelos de calzado infantil y calzado para adultos.</p>
-            {/* <a className="btn btn-primary btn-lg" href="#products-container">Ver Productos</a> */}
             <Link className="btn btn-primary btn-lg" to='/productos'>Ver Productos</Link>
         </div>
     );
