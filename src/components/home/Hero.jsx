5825a9cda86a694317c730ed7ec83cc8428406a3
blackanvilsoftworks <blackanvilsoftworks@gmail.com>
2026-01-18 20:30:28 -0300

    Se actualiza el componente Hero para hacer uso del usContext. Se actualiza la constante imagesPath

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,9 +1,15 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import './Hero.scss';
+import { DataContext } from '../../contexts/Data.jsx';
 
 const Hero = () => {
+    const { objContainers } = useContext(DataContext);
+        
+    const data = objContainers.hero;
+
     return (
-        <div id="hero_container" className="hero mt-5 px-3">
+        <div id={data.id} className={data.className}>
             <h1 className="display-4 fw-bold mb-4">Calzados para Toda la Familia</h1>
             <p className="lead mb-4 fw-semibold">Somos distribuidores de una gran variedad de modelos de calzado infantil y calzado para adultos.</p>
             <Link className="btn main-btn-style btn-lg" to='/productos'>Ver Productos</Link>
