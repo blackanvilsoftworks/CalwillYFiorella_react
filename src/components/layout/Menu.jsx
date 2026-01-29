7b84e88c416a95da45bd93e65d5ea9881170630d
blackanvilsoftworks <blackanvilsoftworks@gmail.com>
2026-01-29 20:04:11 -0300

    Se ingresa al componente Menu, se crean referencias para las etiquetas afectadas y se genera una función collapseMenu que salta al presionar cualquier link, evalúa si el menú está en estado de montado, y si lo está, deja los atributos en el estado de desmontado.

--- src/components/layout/Menu.jsx
+++ src/components/layout/Menu.jsx
@@ -5,7 +5,7 @@ import {
 } from 'react-router-dom';
 
 import './Menu.scss';
-import { useContext } from 'react';
+import { useContext, useRef } from 'react';
 
 const Menu = () => {
 
@@ -18,27 +18,38 @@ const Menu = () => {
         { name: 'Contacto'              , path: '/contacto'     },
     ];
 
-    return (<>
+    const menuBtn   = useRef(null);
+    const menuList  = useRef(null);
+
+    const collpaseMenu = () => {
+        if (!menuBtn.current.classList.contains('collapsed')) {
+            menuBtn.current.classList.add('collapsed');
+            menuBtn.current.setAttribute('aria-expanded', 'false');
+            menuList.current.classList.remove('show');
+        }
+    };
+
+    return (
         <nav id="navbar_container" className="navbar navbar-expand-md navbar-light m-0 p-0 fixed-top shadow">
             <div className="container-fluid">
                 <Link className="navbar-brand" to="/">
                     <img className="d-inline-block align-text-top me-2" src="/navbar-logo.png" alt={`${globalInfo.name} Logo`} height="40" />
                     <span className="ms-2 navbar-title">{globalInfo.name.toUpperCase()}</span>
                 </Link>
-                <button className="navbar-toggler border-0" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" type="button">
+                <button ref={menuBtn} className="navbar-toggler border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" type="button">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div id="navbarNav" className="collapse navbar-collapse text-center ps-auto">
+                <div ref={menuList} id="navbarNav" className="collapse navbar-collapse text-center ps-auto">
                     <ul className="navbar-nav ms-auto">
                         {
                             links.map(({ name, path }, i) => (
-                                <li key={name} className='nav-item'><NavLink className="nav-link" to={path}>{name}</NavLink></li>
+                                <li key={name} className='nav-item'><NavLink className="nav-link" to={path} onClick={collpaseMenu}>{name}</NavLink></li>
                             ))
                         }
                     </ul>
                 </div>
             </div>
         </nav>
-    </>);
+    );
 };
 export default Menu;
